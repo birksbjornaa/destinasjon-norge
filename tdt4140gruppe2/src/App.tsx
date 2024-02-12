020eb243a4b2b9de6a96a607cac906c332eed6c2
Sindre Bjerklund Lindberg <sindrbli@stud.ntnu.no>
2024-02-12 15:09:30 +0100

    feat:Added Destination Component. Co-authored-by: Anine Løkken<aninelok@stud.ntnu.no>, Brinje Marie Haugli<brinjemh@stud.ntnu.no>

--- tdt4140gruppe2/src/App.tsx
+++ tdt4140gruppe2/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
-import Home from './pages/home';
+import "./App.css";
+
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home></Home>
     </div>
   );
 }

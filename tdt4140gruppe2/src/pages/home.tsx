f9f39cd2056d9a569e23936dd79f730dbe1a770e
Elise <elise.flovikholm@icloud.com>
2024-02-12 16:06:32 +0100

    Feat: Working with frontend Created components for frontend and created test folder
    Co-authored by: Birk Bjornaa and Luka Krstic

--- tdt4140gruppe2/src/pages/home.tsx
+++ tdt4140gruppe2/src/pages/home.tsx
@@ -1,6 +1,8 @@
 import { addDoc, collection } from "@firebase/firestore";
 import { SyntheticEvent, useRef } from 'react';
 import { firestore } from "../components/firebaseConfig";
+import Header from "../components/Header";
+import profile from '../assets/profile.jpg';
 
 export default function Home() {
   const messageRef = useRef<HTMLInputElement>(null);
@@ -21,8 +23,17 @@ export default function Home() {
     }
   };
 
+  const handleHomeClick = () => {
+    // Logikken for å navigere tilbake til hjemmesiden
+  };
+  
   return (
     <div>
+       <Header
+        myProfile= {profile}
+        homeButton="logoOgTittel" // Erstatt med stien til din hjem-logo
+        onClick={handleHomeClick}
+      />
       <form onSubmit={handleSave}>
         <label>Enter Message</label>
         <input type="text" ref={messageRef} />

5c5abf92510d65c8fb8d5063fc91780bc88ab0c0
Sindre Bjerklund Lindberg <sindrbli@stud.ntnu.no>
2024-03-04 15:32:56 +0100

    [feat] destination page implemented in filtering page
    get more information about the destinations on the filtering page
    
    Co-authored-by: eliseflo <eliseflo@users.noreply.github.com>

--- src/components/DestinationOverview.tsx
+++ src/components/DestinationOverview.tsx
@@ -2,23 +2,32 @@ import Destination, { DestinationProps } from "./GalleryDestination";
 import "../css/DestinationChooser.css";
 
 export interface MainGalleryProps {
-    destinations: DestinationProps[];
+  destinations: DestinationProps[];
+  handleTileClicked: (destinationID: string) => void;
 }
 
-const DestinationsOverview: React.FC<MainGalleryProps> = ({ destinations }) => {
-    return (
-        <div className="DestinationOverview">
-            {destinations.map((destination) => (
-                <div key={destination.name} className="SingleDestination">
-                    <Destination
-                        name={destination.name}
-                        imageSrc={destination.imageSrc}
-                    />
-                </div>
-            ))}
+const DestinationsOverview: React.FC<MainGalleryProps> = ({
+  destinations,
+  handleTileClicked,
+}) => {
+  return (
+    <div className="DestinationOverview">
+      {destinations.map((destination) => (
+        <div key={destination.name} className="SingleDestination">
+          <button onClick={() => handleTileClicked(destination.id)}>
+            <Destination
+              name={destination.name}
+              imageSrc={destination.imageSrc}
+              key={destination.id}
+              id={destination.id}
+              region={destination.region}
+              description={destination.region}
+            />
+          </button>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default DestinationsOverview;
-

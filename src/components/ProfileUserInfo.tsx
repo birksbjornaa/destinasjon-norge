0f566b998e00140ff8844952a8437132269da3c3
Sindre Bjerklund Lindberg <sindrbli@stud.ntnu.no>
2024-03-18 11:46:22 +0100

    Logic to update tags in database

--- src/components/ProfileUserInfo.tsx
+++ src/components/ProfileUserInfo.tsx
@@ -1,17 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import profilePicture from "../assets/profilePicture.png";
 import MainGallery from "../components/MainGallery";
-import { getAllDestinations } from '../controllers/fierbaseController';
-import '../css/Main.css';
-import '../css/Profile.css';
-import { currentUserEmail } from "../pages/Login";
-import FilteringBar from './FilteringBar';
-import { DestinationProps } from './GalleryDestination';
-
+import { getAllDestinations } from "../controllers/fierbaseController";
+import "../css/Main.css";
+import "../css/Profile.css";
+import { currentToken, currentUserEmail } from "../pages/Login";
+import FilteringBar from "./FilteringBar";
+import { DestinationProps } from "./GalleryDestination";
+import { db } from "../config/firebaseConfig";
+import { collection, doc } from "@firebase/firestore";
+import { updateDoc } from "firebase/firestore";
+import { DocumentData, DocumentReference } from "firebase/firestore";
+import { DatabaseReference } from "firebase/database";
 
 const UserProfile: React.FC = () => {
   const [destinations, setDestinations] = useState<DestinationProps[]>([]);
+  const [tags, setTags] = useState<string[]>([]);
 
   useEffect(() => {
     fetchAndSetData();
@@ -22,23 +27,18 @@ const UserProfile: React.FC = () => {
     setDestinations(destinations);
   };
 
-  const applyFilters = async (tags: string[], price: number) => {
-    const filteredDestinations = (await getAllDestinations()).filter(
-      (destination) => {
-        console.log(destination.tags);
-        return (
-          destination.price <= price &&
-          tags.filter((tag) => !destination.tags.includes(tag)).length == 0
-        );
-      }
-    );
-    setDestinations(filteredDestinations);
+  const applyFilters = (tags: string[], price: number) => {
+    const colRef = collection(db, "Users");
+    const docRef = doc(colRef, currentToken);
+    updateDoc(docRef, {
+      tags: tags,
+    });
   };
-  
+
   // må lage en const for å si hva som skjer når du trykker på knappen ved filterne
   // const saveFiltersToProfile
   const navigate = useNavigate();
-  
+
   const handleDestinationTileClicked = (destinationId: string) => {
     navigate("/destination/" + destinationId);
   };
@@ -52,7 +52,11 @@ const UserProfile: React.FC = () => {
         <div className="email">
           <p>{currentUserEmail}</p>
           <br></br>
-          <p>Bla ned for å se en oversikt over dine foretrukne tags, dine besøkte <br></br>destinasjoner, favorittdestinasjoner og dine egne vurderinger</p>
+          <p>
+            Bla ned for å se en oversikt over dine foretrukne tags, dine besøkte{" "}
+            <br></br>destinasjoner, favorittdestinasjoner og dine egne
+            vurderinger
+          </p>
         </div>
       </div>
       {/* finne en måte vi ikke får med prisnivå? */}
@@ -66,22 +70,22 @@ const UserProfile: React.FC = () => {
         <h2>Besøkte destinasjoner</h2>
         <div className="MainGalleryProfile">
           <MainGallery
-              destinations={destinations}
-              // handleTileClicked={saveFiltersToProfile} til når vi vil lagre dette til bruker
-              handleTileClicked={handleDestinationTileClicked}
-              neverShowArrows={false}
-            />
-            </div>
+            destinations={destinations}
+            // handleTileClicked={saveFiltersToProfile} til når vi vil lagre dette til bruker
+            handleTileClicked={handleDestinationTileClicked}
+            neverShowArrows={false}
+          />
+        </div>
       </div>
       <div className="favourite">
         <h2>Mine favorittdestinasjoner</h2>
         <div className="MainGalleryProfile">
           <MainGallery
-              destinations={destinations}
-              // handleTileClicked={saveFiltersToProfile}
-              handleTileClicked={handleDestinationTileClicked}
-              neverShowArrows={false}
-            />
+            destinations={destinations}
+            // handleTileClicked={saveFiltersToProfile}
+            handleTileClicked={handleDestinationTileClicked}
+            neverShowArrows={false}
+          />
         </div>
       </div>
       <div>
